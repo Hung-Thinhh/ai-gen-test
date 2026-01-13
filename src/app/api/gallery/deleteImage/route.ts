71bb00f3db7a138aa45a69587c125824637dbac3
Hung-Thinhh <hungthinhh2003@gmail.com>
2026-01-13 18:21:41 +0700

    v4 fix

--- src/app/api/gallery/deleteImage/route.ts
+++ src/app/api/gallery/deleteImage/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { getServerSession } from 'next-auth';
-import { authOptions } from '../../../auth/[...nextauth]/route';
+import { authOptions } from '../../auth/[...nextauth]/route';
 import { v2 as cloudinary } from 'cloudinary';
 import { extractPublicIdFromUrl, isCloudinaryUrl } from '@/utils/cloudinaryUtils';
 
